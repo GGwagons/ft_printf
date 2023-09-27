eb0718c7809e7290e5f1504dcad28d52f9b9b7e2
GGwagons <108860675+GGwagons@users.noreply.github.com>
2023-09-27 20:33:32 +0200

    Update ft_putu.c

--- ft_printf/ft_putu.c
+++ ft_printf/ft_putu.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putu.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miturk <miturk@student.42.fr>              +#+  +:+       +#+        */
+/*   By: miturk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/21 15:23:13 by miturk            #+#    #+#             */
-/*   Updated: 2023/09/21 15:37:13 by miturk           ###   ########.fr       */
+/*   Created: 2023/09/25 08:47:35 by miturk            #+#    #+#             */
+/*   Updated: 2023/09/25 08:47:36 by miturk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,9 @@ int	ft_putu(unsigned int nbr)
 		len = len + ft_putu(nbr % 10);
 	}
 	else
+	{
 		ft_putchar_p(nbr + '0');
+		len++;
+	}
 	return (len);
 }
